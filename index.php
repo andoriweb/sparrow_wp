3b9a9b650d18304dfae2b379ae2c39c6ad723be4
andoriweb <andoriweb@gmail.com>
2021-05-28 15:08:59 +0300

    slider

--- index.php
+++ index.php
@@ -15,39 +15,42 @@ Template Name: Home
 
     <ul class="slides">
 
+      <?php 
+     // параметры по умолчанию
+      $posts = get_posts( array(
+        'numberposts' => 0,
+        'category'    => 23,
+        'orderby'     => 'date',
+        'order'       => 'ASC',
+        'include'     => array(),
+        'exclude'     => array(),
+        'meta_key'    => '',
+        'meta_value'  =>'',
+        'post_type'   => 'post',
+        'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+      ) );
+
+      foreach( $posts as $post ){
+        setup_postdata($post);
+        ?>
       <!-- Slide -->
       <li>
         <div class="row">
           <div class="twelve columns">
             <div class="slider-text">
-              <h1>Free amazing site template<span>.</span></h1>
-              <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
-                    enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis, dolores et quas molestias excepturi
-                    enim tellus ultrices elit.</p>
+              <h1><?php the_title(); ?><span>.</span></h1>
+              <p><?php the_content(); ?></p>
             </div>
-                  <div class="slider-image">
-                    <img src="images/sliders/home-slider-image-01.png" alt="" />
-                  </div>
+            <div class="slider-image">
+              <?php the_post_thumbnail( 'thumbnails' ); ?>
+            </div>
           </div>
         </div>
       </li>
-
-        <!-- Slide -->
-      <li>
-        <div class="row">
-          <div class="twelve columns">
-            <div class="slider-text">
-              <h1>Responsive + HTML5 + CSS3<span>.</span></h1>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
-                    deleniti eos et accusamus. amet consequat enim elit noneas sit amet luctu. lacus sit amet luctus lobortis.
-                    Aenean condimentum, lacus sit amet luctus.</p>
-            </div>
-                  <div class="slider-image">
-                    <img src="images/sliders/home-slider-image-02.png" alt="" />
-                  </div>
-          </div>
-        </div>
-      </li>
+      <?php
+      } 
+      wp_reset_postdata(); // сброс
+     ?>
 
     </ul>
 
@@ -61,41 +64,41 @@ Template Name: Home
 
   <div class="row">
 
-      <div class="bgrid-quarters s-bgrid-halves">
+    <div class="bgrid-quarters s-bgrid-halves">
 
-        <div class="columns">
-          <h2>Clean & Modern.</h2>
+      <div class="columns">
+        <h2>Clean & Modern.</h2>
 
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </p>
-        </div>
+        </p>
+      </div>
 
-        <div class="columns">
-          <h2>Responsive.</h2>
+      <div class="columns">
+        <h2>Responsive.</h2>
 
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </p>
-        </div>
+        </p>
+      </div>
 
-        <div class="columns s-first">
-          <h2>HTML5 + CSS3.</h2>
+      <div class="columns s-first">
+        <h2>HTML5 + CSS3.</h2>
 
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </p>
-        </div>
+        </p>
+      </div>
 
-        <div class="columns">
-          <h2>Free of Charge.</h2>
+      <div class="columns">
+        <h2>Free of Charge.</h2>
 
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
           Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </p>
-        </div>
+        </p>
+      </div>
 
-        </div>
+    </div>
 
   </div>
 
@@ -107,69 +110,69 @@ Template Name: Home
 
   <div class="row">
 
-      <div class="twelve columns align-center">
-        <h1>Some of our recent works.</h1>
-      </div>
+    <div class="twelve columns align-center">
+      <h1>Some of our recent works.</h1>
+    </div>
 
-      <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-halves">
+    <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-halves">
 
-        <div class="columns portfolio-item">
-            <div class="item-wrap">
-            <a href="portfolio.html">
-                  <img alt="" src="images/portfolio/geometrics.jpg">
-                  <div class="overlay"></div>
-                  <div class="link-icon"><i class="fa fa-link"></i></div>
-              </a>
+      <div class="columns portfolio-item">
+        <div class="item-wrap">
+          <a href="portfolio.html">
+            <img alt="" src="images/portfolio/geometrics.jpg">
+            <div class="overlay"></div>
+            <div class="link-icon"><i class="fa fa-link"></i></div>
+          </a>
           <div class="portfolio-item-meta">
-              <h5><a href="portfolio.html">Geometrics</a></h5>
-                  <p>Illustration</p>
+            <h5><a href="portfolio.html">Geometrics</a></h5>
+            <p>Illustration</p>
           </div>
-            </div>
+        </div>
       </div>
 
-        <div class="columns portfolio-item">
-            <div class="item-wrap">
-            <a href="portfolio.html">
-                  <img alt="" src="images/portfolio/console.jpg">
-                  <div class="overlay"></div>
-                  <div class="link-icon"><i class="fa fa-link"></i></div>
-              </a>
+      <div class="columns portfolio-item">
+        <div class="item-wrap">
+          <a href="portfolio.html">
+            <img alt="" src="images/portfolio/console.jpg">
+            <div class="overlay"></div>
+            <div class="link-icon"><i class="fa fa-link"></i></div>
+          </a>
           <div class="portfolio-item-meta">
-              <h5><a href="portfolio.html">Console</a></h5>
-                  <p>Web Development</p>
+            <h5><a href="portfolio.html">Console</a></h5>
+            <p>Web Development</p>
           </div>
-            </div>
+        </div>
       </div>
 
-        <div class="columns portfolio-item s-first">
-            <div class="item-wrap">
-            <a href="portfolio.html">
-                  <img alt="" src="images/portfolio/camera-man.jpg">
-                  <div class="overlay"></div>
-                  <div class="link-icon"><i class="fa fa-link"></i></div>
-              </a>
+      <div class="columns portfolio-item s-first">
+        <div class="item-wrap">
+          <a href="portfolio.html">
+            <img alt="" src="images/portfolio/camera-man.jpg">
+            <div class="overlay"></div>
+            <div class="link-icon"><i class="fa fa-link"></i></div>
+          </a>
           <div class="portfolio-item-meta">
-              <h5><a href="portfolio.html">Camera Man</a></h5>
-                  <p>Photography</p>
+            <h5><a href="portfolio.html">Camera Man</a></h5>
+            <p>Photography</p>
           </div>
-            </div>
+        </div>
       </div>
 
-        <div class="columns portfolio-item">
-            <div class="item-wrap">
-            <a href="portfolio.html">
-                  <img alt="" src="images/portfolio/into-the-light.jpg">
-                  <div class="overlay"></div>
-                  <div class="link-icon"><i class="fa fa-link"></i></div>
-              </a>
+      <div class="columns portfolio-item">
+        <div class="item-wrap">
+          <a href="portfolio.html">
+            <img alt="" src="images/portfolio/into-the-light.jpg">
+            <div class="overlay"></div>
+            <div class="link-icon"><i class="fa fa-link"></i></div>
+          </a>
           <div class="portfolio-item-meta">
-              <h5><a href="portfolio.html">Into The Light</a></h5>
-                  <p>Branding</p>
+            <h5><a href="portfolio.html">Into The Light</a></h5>
+            <p>Branding</p>
           </div>
-            </div>
+        </div>
       </div>
 
-      </div>
+    </div>
 
   </div>
 
@@ -180,9 +183,9 @@ Template Name: Home
 <section id="journal">
 
   <div class="row">
-      <div class="twelve columns align-center">
-        <h1>Our latest posts and rants.</h1>
-      </div>
+    <div class="twelve columns align-center">
+      <h1>Our latest posts and rants.</h1>
+    </div>
   </div>
 
   <div class="blog-entries">
@@ -198,31 +201,31 @@ Template Name: Home
       foreach( $posts as $post ){
         setup_postdata($post);
         ?>
-          <article class="row entry">
-            <div class="entry-header">
-                <div class="permalink">
-                  <a href="<?php the_permalink(); ?>"><i class="fa fa-link"></i></a>
-                </div>
-                <div class="ten columns entry-title pull-right">
-                  <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                </div>
-                <div class="two columns post-meta end">
-                  <p>
-                  <time datetime="2014-01-31" class="post-date" pubdate=""><?php echo get_the_date('F j, Y'); ?></time>
-                  <span class="dauthor">By <?php the_author(); ?></span>
-                  </p>
-                </div>
-            </div>
-            <div class="ten columns offset-2 post-content">
-                <?php the_excerpt(  ); ?>
-                <a class="more-link" href="<?php the_permalink(); ?>">Read More<i class="fa fa-arrow-circle-o-right"></i></a>
-            </div>
-          </article> <!-- Entry End -->
-        <?php
+    <article class="row entry">
+      <div class="entry-header">
+        <div class="permalink">
+          <a href="<?php the_permalink(); ?>"><i class="fa fa-link"></i></a>
+        </div>
+        <div class="ten columns entry-title pull-right">
+          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        </div>
+        <div class="two columns post-meta end">
+          <p>
+            <time datetime="2014-01-31" class="post-date" pubdate=""><?php echo get_the_date('F j, Y'); ?></time>
+            <span class="dauthor">By <?php the_author(); ?></span>
+          </p>
+        </div>
+      </div>
+      <div class="ten columns offset-2 post-content">
+        <?php the_excerpt(  ); ?>
+        <a class="more-link" href="<?php the_permalink(); ?>">Read More<i class="fa fa-arrow-circle-o-right"></i></a>
+      </div>
+    </article> <!-- Entry End -->
+    <?php
       } 
       wp_reset_postdata(); // сброс
      ?>
-      
+
   </div> <!-- Entries End -->
 
 </section> <!-- Journal Section End-->
@@ -233,20 +236,22 @@ Template Name: Home
 
   <div class="row">
 
-      <div class="eight columns offset-1">
+    <div class="eight columns offset-1">
 
-        <h1><a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT">Host This Template on Dreamhost.</a></h1>
-        <p>Looking for an awesome and reliable webhosting? Try <a href="http://www.dreamhost.com"><span>DreamHost</span></a>.
-      Get <span>$50 off</span> when you sign up with the promocode <span>STYLESHOUT</span>. 
-      <!-- Simply type	the promocode in the box labeled “Promo Code” when placing your order. --></p>
+      <h1><a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT">Host This Template on Dreamhost.</a></h1>
+      <p>Looking for an awesome and reliable webhosting? Try <a
+          href="http://www.dreamhost.com"><span>DreamHost</span></a>.
+        Get <span>$50 off</span> when you sign up with the promocode <span>STYLESHOUT</span>.
+        <!-- Simply type	the promocode in the box labeled “Promo Code” when placing your order. -->
+      </p>
 
-      </div>
+    </div>
 
-      <div class="three columns action">
+    <div class="three columns action">
 
-        <a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT" class="button">Sign Up Now</a>
+      <a href="http://www.dreamhost.com/r.cgi?287326|STYLESHOUT" class="button">Sign Up Now</a>
 
-      </div>
+    </div>
 
   </div>
 
