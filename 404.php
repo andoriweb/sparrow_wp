19bf0e01364153a567b39381a182a20ca7648228
andoriweb <andoriweb@gmail.com>
2021-05-31 11:25:43 +0300

    page 404

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+
+<section>
+  <div class="container">
+    <h1 class='error'>
+      404<br>Ошибка!!!<br>
+      Такой страницы нет!!!
+    </h1>
+  </div>
+</section>
+
+<?php get_footer() ?>
